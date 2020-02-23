390aa80b491c9fd121c0ba86cb4e52bac4b8ed3d
Rasheed <a.rasheedalabi@gmail.com>
2020-02-24 00:04:22 +0100

    serve static page

---
+++ precache-manifest.e2af1c3e1cc42a0c76b2ad513adf621a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c9b219b13adc81664defe15f3488f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45110b735de926ef282b",
+    "url": "/static/css/main.c01f1cf5.chunk.css"
+  },
+  {
+    "revision": "d5b2c23e9918d81f89ab",
+    "url": "/static/js/2.c30b3de5.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.c30b3de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45110b735de926ef282b",
+    "url": "/static/js/main.03a35ce3.chunk.js"
+  },
+  {
+    "revision": "dcf4a8f74794e3e6d99b",
+    "url": "/static/js/runtime-main.8c9882f9.js"
+  },
+  {
+    "revision": "dc2411c98b969c2d71b0920ae3e2d9d8",
+    "url": "/static/media/bg.dc2411c9.jpg"
+  },
+  {
+    "revision": "35f3a5aec6a2026d1f96de322219136d",
+    "url": "/static/media/logo.35f3a5ae.svg"
+  },
+  {
+    "revision": "fab25bc378670766a10e1ec37712945e",
+    "url": "/static/media/tab-1-pic.fab25bc3.png"
+  },
+  {
+    "revision": "66c7c1fb5b71b2deaff929bbcd3bacf7",
+    "url": "/static/media/tab-macbook.66c7c1fb.png"
+  },
+  {
+    "revision": "dc986d819c053deb6180a34320af2988",
+    "url": "/static/media/tab-tablet.dc986d81.png"
+  },
+  {
+    "revision": "9f91203b7c374a590dd727c323d5b6dc",
+    "url": "/static/media/tab-tv.9f91203b.png"
+  }
+]);
